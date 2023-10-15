0ecddbd94bd165234b4569bc338b6cea9a1eee21
Brock Williams <b.williams54255@gmail.com>
2023-10-14 23:50:05 -0700

    made changes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -15,4 +15,4 @@ app.use('/api', apiRoutes);
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
